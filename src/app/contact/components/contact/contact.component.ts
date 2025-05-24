168a57c7b02717adc11c650d4bae855ac63b565f
Jobi S S <jobisjobi1234@gmail.com>
2025-05-24 22:34:06 +0530

    refactor: update styles import and remove unused components
    - Replace `@import` with `@use` in styles.scss for better module encapsulation
    - Remove unused HeaderComponent and FooterComponent imports from contact.component.ts

--- src/app/contact/components/contact/contact.component.ts
+++ src/app/contact/components/contact/contact.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
 import { ContactFormComponent } from '../contact-form/contact-form.component';
-import { HeaderComponent } from '../../../shared/components/header/header.component';
-import { FooterComponent } from '../../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-contact',
   imports: [ContactFormComponent,
-    HeaderComponent,
-    FooterComponent
   ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
